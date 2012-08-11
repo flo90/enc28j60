41ad775c4ec5e1914bd4ea7cc7b025b13d5615e9
Florian Menne <florianmenne@t-online.de>
2012-08-11 02:51:49 +0200

    Improved code

--- enc28j60.c
+++ enc28j60.c
@@ -4,16 +4,11 @@
 #include "enc28j60.h"
 #include "enc28j60buffer.h"
 #include "spi.h"
+#include "usart.h"
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-#ifdef ENC28J60_DEBUG
-#include <stdio.h>
-char dbgmsgbuf[30];
-void (*dbgmsgout) (char *msg);
-#endif
 
 
 uint8_t (*exchangebyte) (uint8_t data);
@@ -34,139 +29,111 @@ static inline void deselect(void)
   PORTB |= ( 1 << PB4 );
 }
 
-static inline void bitFieldSet( uint8_t reg, uint8_t mask )
+static inline void enc28j60_writeOp( uint8_t op, uint8_t reg, uint8_t data )
 {
   select();
   
-  exchangebyte ( ( BFS ) | reg );
-  exchangebyte( mask );
+  exchangebyte( op | ( reg & ENC28j60_NOREGEXTENSION ) );
+  exchangebyte( data );
   
   deselect();
 }
 
-static inline void bitFieldClear( uint8_t reg, uint8_t mask )
+static inline uint8_t enc28j60_readOp( uint8_t op, uint8_t reg )
 {
+  uint8_t data;
+  
   select();
   
-  exchangebyte( ( BFC ) | reg );
-  exchangebyte( mask );
+  exchangebyte( op | ( reg & ENC28j60_NOREGEXTENSION ) );
+  
+  data = exchangebyte( 0x00 );
+  
+  if( !( reg & 0x80 ) )
+  {
+    data = exchangebyte( 0x00 );
+  }
   
   deselect();
+  
+  return data;
 }
 
 static inline void setbank( uint8_t bank )
 {
   bank &= 0x03;
   
-  bitFieldClear( ( ECON1 & NOREGEXTENSION ), 0x03 );
+  enc28j60_writeOp( ENC28j60_BFC, ENC28j60_ECON1, 0x03); 
   
-  bitFieldSet( ( ECON1 & NOREGEXTENSION ), bank );
+  enc28j60_writeOp( ENC28j60_BFS, ENC28j60_ECON1, bank );
 }
 
-static inline void initRecvBuffer(void)
+void enc28j60_writeReg( uint8_t reg, uint8_t  data )
 {
-  //Writing start Pointer of RX BUFFER
-  enc28j60_writeReg( ERXSTL, RXBUFFERSTPTR & 0x00FF );
-  enc28j60_writeReg( ERXSTH, RXBUFFERSTPTR >> 8 );
-
-#ifdef ENC28J60_DEBUG
-  snprintf(dbgmsgbuf, 20, "RXBUFSTPTR %x\n\r", RXBUFFERSTPTR);
-  dbgmsgout( dbgmsgbuf );
-#endif
+  uint8_t tempbank = ( ( reg & 0x60 ) >> 5 );
   
-  //Writing end Pointer of RX BUFFER
-  enc28j60_writeReg( ERXNDL, RXBUFFERNDPTR & 0x00FF );
-  enc28j60_writeReg( ERXNDH, RXBUFFERNDPTR >> 8 );
+  if( ( ( reg & ENC28j60_NOREGEXTENSION ) < ( ENC28j60_EIE & ENC28j60_NOREGEXTENSION ) ) && ( lastbank != tempbank ) )
+  {
+    setbank( tempbank );
+    lastbank = tempbank;
+  } 
   
-#ifdef ENC28J60_DEBUG
-  snprintf(dbgmsgbuf, 20, "RXBUFNDPTR %x\n\r", RXBUFFERNDPTR);
-  dbgmsgout( dbgmsgbuf );
-#endif
-  
-  //Bug workaround ERXRDPTR may mess up recive Buffer if it contains an even address
-  enc28j60_writeReg( ERXRDPTL, RXBUFFERNDPTR & 0x00FF );
-  enc28j60_writeReg( ERXRDPTH, RXBUFFERNDPTR >> 8 );
+  enc28j60_writeOp( ENC28j60_WCR, reg, data );
   
 }
 
-static inline void initRecvFilter(void)
+uint8_t enc28j60_readReg( uint8_t reg )
 {
-  //We dont need to change anything, the default Values only accept Packets with local MAC adress an Broadcast adress
+  uint8_t tempbank = ( reg & 0x60 ) >> 5;
+  
+  if( ( ( reg & ENC28j60_NOREGEXTENSION ) < ( ENC28j60_EIE & ENC28j60_NOREGEXTENSION ) ) && ( lastbank != tempbank ) )
+  {
+    setbank( tempbank );
+    lastbank = tempbank;
+  } 
+  
+  return enc28j60_readOp( ENC28j60_RCR, reg );
 }
 
-static inline void initMAC( unsigned char mac[6] )
+void enc28j60_writePhyReg( uint8_t reg, uint16_t data )
 {
   
-  //This clears the MARST bit to wake up the MAC - Bit Manipulation is unnessary because all other bits are zero
-  enc28j60_writeReg( MACON2, 0x00 );
-  
-  //Like the Datasheet describe we set MARXEN to enable the MAC an TXPAUS and RXPAUS for flow control
-  enc28j60_writeReg( MACON1, MACON1_MARXEN );
-  
-  //Now the Padding is configured. The best way is to configure automatic padding with adding zeros for small packets an CRC checksum
-  //the pattern for this configuration is 101 for PADCFG2:PADCFG1 furthermore fulduplex is enabled
-  enc28j60_writeReg( MACON3, MACON3_PADCFG2 | MACON3_PADCFG0 );
-  
-  //You may want to change the Maximum Packet length for RX, TX
-  //enc28j60_writeReg( MAMXFLL, 0x00 );
-  //enc28j60_writeReg( MAMXFLH, 0x06 );
-  
-  //MABBIPG: Programm with 15h for fullduplex and 12h for halfduplex
-  enc28j60_writeReg( MABBIPG, 0x12 );
-  
-  //MAIPGL: Datasheet recommends 12h
-  enc28j60_writeReg( MAIPGL, 0x12 );
-  
-  //MAIPGH: Programm this if halfduplex is used Datasheet recommends 0Ch
-  enc28j60_writeReg( MAIPGH, 0x0C );
-  
-  //MACLCON 1, 2: If halfduplex you can change this values but the reset values are OK
-  //It's recommended to increase the MACLCON2 value if long cables are used
-  //enc28j60_writeReg( MACLCON1, 0x0F );
-  //enc28j60_writeReg( MACLCON2, 0x37 );
-  
-  //Setting MAC Adress
-  enc28j60_writeReg( MAADR0,  mac[0] );
-  enc28j60_writeReg( MAADR1,  mac[1] );
-  enc28j60_writeReg( MAADR2,  mac[2] );
-  enc28j60_writeReg( MAADR3,  mac[3] );
-  enc28j60_writeReg( MAADR4,  mac[4] );
-  enc28j60_writeReg( MAADR5,  mac[5] );
-  
+ enc28j60_writeReg( ENC28j60_MIREGADR, reg );
+ enc28j60_writeReg( ENC28j60_MIWRL, data );
+ enc28j60_writeReg( ENC28j60_MIWRH, ( data >> 8 ) );
+ 
+ while( enc28j60_readReg( ENC28j60_MISTAT ) & ENC28j60_MISTAT_BUSY );
 }
 
-static inline void initPHY(void)
+uint16_t enc28j60_readPhyReg ( uint8_t reg )
 {
-  uint16_t temp;
+  uint8_t micmdtemp;
+  uint16_t result=0;
   
-  //We get the default config of PHCON1
-  temp = enc28j60_readPhyReg( PHCON1 );
+  enc28j60_writeReg( ENC28j60_MIREGADR, reg );
+  micmdtemp = enc28j60_readReg ( ENC28j60_MICMD );
+  micmdtemp |= ENC28j60_MICMD_MIIRD;
+  enc28j60_writeReg( ENC28j60_MICMD, micmdtemp );
   
-  //Activating PDPXMD
-  temp &= ~PHCON1_PDPXMD;
+  while( enc28j60_readReg( ENC28j60_MISTAT ) & ENC28j60_MISTAT_BUSY );
   
-  //Writing back manipulated value
-  enc28j60_writePhyReg( PHCON1, temp );
+  micmdtemp &= ~ENC28j60_MICMD_MIIRD;
   
-  //You may change the LED output behavior
-  enc28j60_writePhyReg(PHLCON, 0x3122 );
+  enc28j60_writeReg( ENC28j60_MICMD, micmdtemp );
   
+  result |= enc28j60_readReg( ENC28j60_MIRDL );
+  result |= ( enc28j60_readReg( ENC28j60_MIRDH ) << 8 );
   
-  
+  return result;
 }
 
-#ifndef ENC28J60_DEBUG
 void enc28j60_init( uint8_t (*pexchangebyte) (uint8_t data), unsigned char plocalmac[6] )
 {
   uint8_t i;
   
   exchangebyte = pexchangebyte;
-  
-  select();
-  
-  exchangebyte( SOFTRESET );
-  
-  deselect();
+  _delay_ms(2);
+  enc28j60_writeOp( ENC28j60_SOFTRESET, 0, ENC28j60_SOFTRESET );
   
   lastbank=0;
   
@@ -176,166 +143,125 @@ void enc28j60_init( uint8_t (*pexchangebyte) (uint8_t data), unsigned char ploca
   //Wait for ESTAT_CLKRDY
   //Not reliable!!!!! Errata issue 2
   //while( !( enc28j60_readReg( ESTAT ) & ESTAT_CLKRDY ) );
-  _delay_ms(1);
+  _delay_ms(2);
   
   //Now we init the MAC like described in the Datasheet
   
-  initRecvBuffer();
   
+  
+  // START: Init Recv Buffer
+  //Writing start Pointer of RX BUFFER
+  enc28j60_writeReg( ENC28j60_ERXSTL, RXBUFFERSTPTR & 0x00FF );
+  enc28j60_writeReg( ENC28j60_ERXSTH, RXBUFFERSTPTR >> 8 );
+
+  
+  //Writing end Pointer of RX BUFFER
+  enc28j60_writeReg( ENC28j60_ERXNDL, RXBUFFERNDPTR & 0x00FF );
+  enc28j60_writeReg( ENC28j60_ERXNDH, RXBUFFERNDPTR >> 8 );
+  
+  
+  //Bug workaround ERXRDPTR may mess up recive Buffer if it contains an even address
+  enc28j60_writeReg( ENC28j60_ERXRDPTL, RXBUFFERNDPTR & 0x00FF );
+  enc28j60_writeReg( ENC28j60_ERXRDPTH, RXBUFFERNDPTR >> 8 );
+  
+  // END: Init Recv Buffer
+  
+  
+  
+  // START: Init Recv Filter
   
   //For default operation we dont need to change anything. Description in Line 59
-  //initRecvFilter();
   
-  //init the Media Access Controller
-  initMAC( plocalmac );
+  //We dont need to change anything, the default Values only accept Packets with local MAC adress and Broadcast adress
   
-  for( i = 0; i < 6; i++)
-  {
-    localmac[i] = plocalmac[i];
-  }
-  
-  initPHY();
-  
-}
-#endif
-
-#ifdef ENC28J60_DEBUG
-
-void enc28j60_init( uint8_t (*pexchangebyte) (uint8_t data), void (*pdbgmsgout) (char *msg), unsigned char plocalmac[6] )
-{
-  uint8_t i;
-  
-  exchangebyte = pexchangebyte;
-  dbgmsgout = pdbgmsgout;
-  
-  select();
-  
-  exchangebyte( SOFTRESET );
-  
-  deselect();
-  
-  lastbank=0;
-  
-  nextPacketPtr = 0;
-  
-  //Wait for ESTAT_CLKRDY 
-  //while( !( enc28j60_readReg( ESTAT ) & ESTAT_CLKRDY ) );
-  _delay_ms(1);
-  
-  dbgmsgout( "MAC init\n\r" );
-  
-  //Now we init the MAC like described in the Datasheet
-  
-  initRecvBuffer();
+  // END: Init Recv Filter
   
   
-  //For default operation we dont need to change anything. Description in Line 59
-  //initRecvFilter();
   
-  //init the Media Access Controller
-  initMAC( plocalmac );
-  
-  for( i = 0; i < 6; i++)
-  {
-    localmac[i] = plocalmac[i];
-  }
-  
-  initPHY();
-  
-  bitFieldSet( ECON1, ECON1_TXRST );
-  
-}
-
-#endif
-
-uint8_t enc28j60_readReg( uint8_t reg )
-{
-  uint8_t result;
-  uint8_t tempbank = ( reg & 0x60 ) >> 5;
-  
-  select();
-  
-  if( ( ( reg & NOREGEXTENSION ) < ( EIE & NOREGEXTENSION ) ) && ( lastbank != tempbank ) )
-  {
-    setbank( tempbank );
-    lastbank = tempbank;
-  } 
-  
-  exchangebyte( RCR | ( reg & NOREGEXTENSION ) );
-  
-  if( reg & ETH )
-  {   
-     result = exchangebyte( DONT_CARE ); 
-  }
-  else
-  {
-    exchangebyte( DONT_CARE );
-    result = exchangebyte( DONT_CARE );
-  }
-  
-  deselect();
-  
-  return result;
-}
-
-void enc28j60_writeReg( uint8_t reg, uint8_t  data )
-{
-  uint8_t tempbank = ( ( reg & 0x60 ) >> 5 );
-  
-  if( lastbank != tempbank )
-  {
-    setbank( tempbank );
-    lastbank = tempbank;
-  }
-  
-  select();
-  
-  exchangebyte( WCR | ( reg & NOREGEXTENSION ) );
-  exchangebyte( data );
-  
-  deselect();
-}
-
-uint16_t enc28j60_readPhyReg ( uint8_t reg )
-{
-  uint8_t micmdtemp;
-  uint16_t result=0;
-  
-  enc28j60_writeReg( MIREGADR, reg );
-  micmdtemp = enc28j60_readReg ( MICMD );
-  micmdtemp |= MICMD_MIIRD;
-  enc28j60_writeReg( MICMD, micmdtemp );
-  
-  while( enc28j60_readReg( MISTAT ) & MISTAT_BUSY );
-  
-  micmdtemp &= ~MICMD_MIIRD;
-  
-  enc28j60_writeReg( MICMD, micmdtemp );
-  
-  result |= enc28j60_readReg( MIRDL );
-  result |= ( enc28j60_readReg( MIRDH ) << 8 );
-  
-  return result;
-}
-
-void enc28j60_writePhyReg( uint8_t reg, uint16_t data )
-{
-  
- enc28j60_writeReg( MIREGADR, reg );
- enc28j60_writeReg( MIWRL, data );
- enc28j60_writeReg( MIWRH, ( data >> 8 ) );
+  // START: Init the Media Access Controller
  
- while( enc28j60_readReg( MISTAT ) & MISTAT_BUSY );
+  //This clears the MARST bit to wake up the MAC - Bit Manipulation is unnessary because all other bits are zero
+  enc28j60_writeReg( ENC28j60_MACON2, 0x00 );
+  
+  //Like the Datasheet describe we set MARXEN to enable the MAC an TXPAUS and RXPAUS for flow control
+  //enc28j60_writeReg( MACON1, MACON1_MARXEN );
+  
+  //Now the Padding is configured. The best way is to configure automatic padding with adding zeros for small packets an CRC checksum
+  //the pattern for this configuration is 101 for PADCFG2:PADCFG1
+  enc28j60_writeReg( ENC28j60_MACON3, ENC28j60_MACON3_PADCFG2 | ENC28j60_MACON3_PADCFG0 );
+  
+  //You may want to change the Maximum Packet length for RX, TX
+  //enc28j60_writeReg( MAMXFLL, 0x00 );
+  //enc28j60_writeReg( MAMXFLH, 0x06 );
+  
+  //MABBIPG: Programm with 15h for fullduplex and 12h for halfduplex
+  enc28j60_writeReg( ENC28j60_MABBIPG, 0x12 );
+  
+  //MAIPGL: Datasheet recommends 12h
+  enc28j60_writeReg( ENC28j60_MAIPGL, 0x12 );
+  
+  //MAIPGH: Programm this if halfduplex is used Datasheet recommends 0Ch
+  enc28j60_writeReg( ENC28j60_MAIPGH, 0x0C );
+  
+  //MACLCON 1, 2: If halfduplex you can change this values but the reset values are OK
+  //It's recommended to increase the MACLCON2 value if long cables are used
+  //enc28j60_writeReg( MACLCON1, 0x0F );
+  //enc28j60_writeReg( MACLCON2, 0x37 );
+  
+  //Setting MAC Adress
+  enc28j60_writeReg( ENC28j60_MAADR0,  plocalmac[0] );
+  enc28j60_writeReg( ENC28j60_MAADR1,  plocalmac[1] );
+  enc28j60_writeReg( ENC28j60_MAADR2,  plocalmac[2] );
+  enc28j60_writeReg( ENC28j60_MAADR3,  plocalmac[3] );
+  enc28j60_writeReg( ENC28j60_MAADR4,  plocalmac[4] );
+  enc28j60_writeReg( ENC28j60_MAADR5,  plocalmac[5] );
+  
+  // END: Init the Media Access Controller
+  
+  for( i = 0; i < 6; i++)
+  {
+    localmac[i] = plocalmac[i];
+  }
+  
+  // START: Init Phy
+  
+ uint16_t temp;
+  
+  //We get the default config of PHCON1
+  temp = enc28j60_readPhyReg( ENC28j60_PHCON1 );
+  
+  //Deactivating PDPXMD - we are in HalfDuplex
+  temp &= ~ENC28j60_PHCON1_PDPXMD;
+  
+  //Writing back manipulated value
+  enc28j60_writePhyReg( ENC28j60_PHCON1, temp );
+  
+  //Preventing Loopback with PHCON2.HDLDIS
+  temp = enc28j60_readPhyReg( ENC28j60_PHCON2 );
+  temp |= ENC28j60_PHCON2_HDLDIS;
+  enc28j60_writePhyReg( ENC28j60_PHCON2, temp );
+  
+  
+  //You may change the LED output behavior
+  enc28j60_writePhyReg( ENC28j60_PHLCON, 0x3122 );
+  
+  // END: Init Phy
+  
 }
 
-static inline void writeBufMem( unsigned char data[],  uint16_t size )
+static inline void enc28j60_setBufWrPtr( uint16_t addr )
+{
+  enc28j60_writeReg( ENC28j60_EWRPTL, addr );
+  enc28j60_writeReg( ENC28j60_EWRPTH, addr >> 8 );
+}
+
+static inline void enc28j60_writeBuffer( unsigned char data[],  uint16_t size )
 { 
   uint16_t i;
   
   select();
   
-  exchangebyte(WBM);
-  
+  exchangebyte( ENC28j60_WBM );
   
   for( i = 0; i < size; i++ )
   {
@@ -345,56 +271,37 @@ static inline void writeBufMem( unsigned char data[],  uint16_t size )
   deselect();
 }
 
-static inline void setTxBufStPtr( uint16_t addr )
-{
-  enc28j60_writeReg( EWRPTL, addr );
-  enc28j60_writeReg( EWRPTH, addr >> 8 );
-}
 
 void enc28j60_sendPacket( unsigned char dst[5],  unsigned char *data, uint16_t size )
 {
-  uint16_t i;
+  uint16_t error_timer;
   
-  setTxBufStPtr( TXBUFFERSTPTR );
   
-  select();
-  
-  exchangebyte(WBM);
+  enc28j60_setBufWrPtr( TXBUFFERSTPTR );
   
   //Control Byte
-  exchangebyte( 0x00 );
+  enc28j60_writeOp( ENC28j60_WBM, 0, 0x00 );
   
   //writing the destination MAC address
-  for( i = 0; i < 6; i++ )
-  {
-    exchangebyte( dst[i] );
-  }
+  enc28j60_writeBuffer( dst, 6 );
   
   //Writing source MAC address
-  for( i = 0; i < 6; i++ )
-  {
-    exchangebyte( localmac[i] );
-  }
+  enc28j60_writeBuffer( localmac, 6 );
   
-  for( i = 0; i < size; i++ )
-  {
-    exchangebyte( data[i] );
-  }
-  
-  deselect();
+  //Writing data
+  enc28j60_writeBuffer( data, size );
   
   //Writing start pointer address for send
-  enc28j60_writeReg( ETXSTL, TXBUFFERSTPTR & 0x00FF );
-  enc28j60_writeReg( ETXSTH, TXBUFFERSTPTR >> 8 );
+  enc28j60_writeReg( ENC28j60_ETXSTL, TXBUFFERSTPTR & 0x00FF );
+  enc28j60_writeReg( ENC28j60_ETXSTH, TXBUFFERSTPTR >> 8 );
   
   //Writing end pointer address for send
-  enc28j60_writeReg( ETXNDL, ( (TXBUFFERSTPTR+12) + size ) & 0x00FF );
-  enc28j60_writeReg( ETXNDH,  ( (TXBUFFERSTPTR+12) + size ) >> 8 );
+  enc28j60_writeReg( ENC28j60_ETXNDL, ( (TXBUFFERSTPTR+12) + size ) & 0x00FF );
+  enc28j60_writeReg( ENC28j60_ETXNDH,  ( (TXBUFFERSTPTR+12) + size ) >> 8 );
   
-  transmissionRestart:
-  
-  enc28j60_writeReg( EWRPTL, ( (TXBUFFERSTPTR+13) + size ) & 0x00FF );
-  enc28j60_writeReg( EWRPTH,  ( (TXBUFFERSTPTR+13) + size ) >> 8 );
+  /*
+  enc28j60_writeReg( ENC28j60_EWRPTL, ( (TXBUFFERSTPTR+13) + size ) & 0x00FF );
+  enc28j60_writeReg( ENC28j60_EWRPTH,  ( (TXBUFFERSTPTR+13) + size ) >> 8 );
   
   select();
   
@@ -406,73 +313,44 @@ void enc28j60_sendPacket( unsigned char dst[5],  unsigned char *data, uint16_t s
   }
   
   deselect();
+  */
   
+  
+  transmissionRestart:
   //Clearing error flags
-  bitFieldClear( EIR, EIR_TXERIF );
-  bitFieldClear( EIR, EIR_TXIF );
-  bitFieldClear( ESTAT, ESTAT_TXABRT );
+  enc28j60_writeOp( ENC28j60_BFC, ENC28j60_EIR, ENC28j60_EIR_TXERIF );
+  enc28j60_writeOp( ENC28j60_BFC, ENC28j60_EIR, ENC28j60_EIR_TXIF );
+  enc28j60_writeOp( ENC28j60_BFC, ENC28j60_ESTAT, ENC28j60_ESTAT_TXABRT );
   
   //Let's go
-  enc28j60_writeReg( ECON1, ECON1_TXRTS );
+  enc28j60_writeOp( ENC28j60_BFS, ENC28j60_ECON1, ENC28j60_ECON1_TXRTS );
   
-  uint8_t error= 255;
-  
-  while( ( ! (enc28j60_readReg( EIR ) & EIR_TXERIF ) ) &&  ( ! (enc28j60_readReg( EIR ) & EIR_TXIF ) )  )
+  error_timer=255;
+  while( enc28j60_readReg( ENC28j60_ECON1 ) & ENC28j60_ECON1_TXRTS  );
   {
-    if(error < 1)
+    //_delay_us(1);
+    if( error_timer  )
     {
-      if(enc28j60_readReg( ESTAT ) & ESTAT_TXABRT)
-      {
-        dbgmsgout("ESTAT_TXABRT\n\r");
-      }
-  
-      if( enc28j60_readReg( EIR ) & EIR_TXIF  )
-      {
-        dbgmsgout("TXIF\n\r");
-      }
-      if( enc28j60_readReg( ECON1 ) & ECON1_TXRTS )
-      {
-       dbgmsgout( "TXRTS\n\r" );
-      }
-      
-       enc28j60_writeReg( ERDPTL, ( (TXBUFFERSTPTR+13) + size ) & 0x00FF );
-       enc28j60_writeReg( ERDPTH,  ( (TXBUFFERSTPTR+13) + size ) >> 8 );
-      
-       select();
-       
-       exchangebyte(RBM);
-       dbgmsgout("Status Vector:\r\n");
-       
-       for( i = 0; i<7; i++)
-       {
-	 snprintf(dbgmsgbuf, 20, "%x\n\r", exchangebyte(0x00));
-         dbgmsgout( dbgmsgbuf );
-       }
-       
-      dbgmsgout( "RST Transmit\n\r" );
-      bitFieldClear( ECON1, ECON1_TXRTS );
-      bitFieldSet( ECON1, ECON1_TXRST );
-      bitFieldClear( ECON1, ECON1_TXRTS );
-      goto transmissionRestart;
+      error_timer = error_timer - 1;
     }
+    
     else
     {
-      --error;
+      usart_puts("Error\r\n");
+      enc28j60_writeOp( ENC28j60_BFC, ENC28j60_ECON1, ENC28j60_ECON1_TXRTS );
+      enc28j60_writePhyReg( ENC28j60_PHLCON, 0x3AA2 );
+      while(1);
+      goto transmissionRestart;
     }
+    
   }
   
-  enc28j60_writeReg( ERDPTL, ( (TXBUFFERSTPTR+13) + size ) & 0x00FF );
-  enc28j60_writeReg( ERDPTH,  ( (TXBUFFERSTPTR+13) + size ) >> 8 );
-
-  select();
-       
-  exchangebyte(RBM);
-  dbgmsgout("Status Vector:\r\n");
-       
-  for( i = 0; i<7; i++)
-  {
-    snprintf(dbgmsgbuf, 20, "%x\n\r", exchangebyte(0x00));
-    dbgmsgout( dbgmsgbuf );
-  }
   
+  
+  
+  /*
+  enc28j60_writeReg( ENC28j60_ERDPTL, ( (TXBUFFERSTPTR+13) + size ) & 0x00FF );
+  enc28j60_writeReg( ENC28j60_ERDPTH,  ( (TXBUFFERSTPTR+13) + size ) >> 8 );
+  */
 }
+
--- enc28j60.h
+++ enc28j60.h
@@ -2,24 +2,16 @@
 #define _ENC28J60_H_
 #include <inttypes.h>
 
-#define ENC28J60_DEBUG
-
-#ifndef ENC28J60_DEBUG
-void enc28j60_init( uint8_t (*pexchangebyte) (uint8_t data), unsigned char macaddr[6] );
-#endif
-
-#ifdef ENC28J60_DEBUG
-void enc28j60_init( uint8_t (*pexchangebyte) (uint8_t data), void (*pdbgmsgout) (char *msg), unsigned char plocalmac[6] );
-#endif
+void enc28j60_writeReg( uint8_t reg, uint8_t  data );
 
 uint8_t enc28j60_readReg( uint8_t reg );
 
-void enc28j60_writeReg( uint8_t reg, uint8_t data );
+void enc28j60_writePhyReg( uint8_t reg, uint16_t data );
 
 uint16_t enc28j60_readPhyReg ( uint8_t reg );
 
-void enc28j60_writePhyReg( uint8_t reg, uint16_t data );
+void enc28j60_init( uint8_t (*pexchangebyte) (uint8_t data), unsigned char plocalmac[6] );
 
-void enc28j60_sendPacket( unsigned char dst[5], unsigned char *data, uint16_t size );
+void enc28j60_sendPacket( unsigned char dst[5],  unsigned char *data, uint16_t size );
 
 #endif
